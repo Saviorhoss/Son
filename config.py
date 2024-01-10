69420f25dbfb3e6d0111f71fa0e3360fa6fb2d8a
Hossein <130118907+Saviorhoss@users.noreply.github.com>
2024-01-10 07:35:07 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
+API_ID = int(os.getenv("API_ID", 14699743))
+API_HASH = os.getenv("API_HASH", "0cef89ed2c8025c16d2b4d42a1b8d792")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "5585143449:AAF_SWZKSBTA6KwzdmewPamUJPQqDcgL07M")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://nohari3255:lK4QeDDOZKefymH0@cluster0.rka3l8x.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "5059280908").split()})
